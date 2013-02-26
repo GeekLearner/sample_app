52121cfdf1450451c0c441b9267baf0924a96478
Amir <amirvalizadeh81@yahoo.com>
2013-02-25 22:48:02 -0800

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,15 +22,18 @@ describe "User Pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      describe "after saving the user" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
 
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+        it "should create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+        it { should have_link('Sign out') }
       end
     end
   end
